c58b93b788a14cfd8da0d1bd732de4265b9fde91
Andrzej Sokolowski <ghoasta18@gmail.com>
2020-04-18 14:24:53 +0100

    another version

--- project.py
+++ project.py
@@ -1,5 +1,41 @@
 import sys #importing sys to be able to exit app on invalid creditentials in login function
 
+
+def create_student_list(filename):
+	student_list = []
+	with open (filename,'r') as f:
+		for line in f:
+			x = (line.split(',')[0])
+			student_list.append(x)
+	return student_list		
+
+
+def take_class_attendance(filename,module_code,purpose):
+	print(f"\nModule Record System({purpose}) {module_code}")
+	print("-"*30)
+	student_list = create_student_list(filename)
+	print(f"There are {len(student_list)} students enrolled.")
+	for i in range(len(student_list)):
+		print(f"Student #{i+1}: {student_list[i]}")
+		print("1. Present")
+		print("2. Absent")
+		print("3. Excused")
+		atten = input("> ")
+
+def get_class_attendance(filename,module_code,purpose):
+	student_list = create_student_list(filename)
+	print(f"Module: {module_code}")
+	print(f"Number of students: {len(student_list)}")
+	print(f"Number of classes: ")
+	print(f"Avarage Attendance: ")
+	print(f"Low Attender(s): under 70.0 %")
+	print(f"")
+	print(f"Non Attender(s):")
+	print(f"")
+	print(f"Best Attender(s):")
+	print(f"")
+
+
 def load_module(purpose):
 	print(f"\nModule Record System({purpose}) -Choose a Module")
 	print("-"*30)	
@@ -10,8 +46,10 @@ def load_module(purpose):
 			modules_list.append(x)	
 	for i in range(len(modules_list)):
 		print(modules_list[i])		
-	menu_module = input("> ")
-	return 
+	menu_module = int(input("> "))
+	filename = modules_list[menu_module-1]+".txt"
+	module_code = modules_list[menu_module-1]
+	return filename,module_code
 
 def login():
 	name = input("\nName: ")
@@ -46,9 +84,13 @@ def main_menu(name): #main function to display main menu
 		menu = input("> ")
 
 		if menu == "1":
-			load_module("Attendance")
+			filename , module_code = load_module("Attendance")
+			#print(f"Filename {filename} | Module Code {module_code}")
+			take_class_attendance(filename,module_code,"Attendance")
 		elif menu == "2":
-			load_module("Statistics")
+			filename , module_code = load_module("Statistics")
+			get_class_attendance(filename,module_code,"Statistics")
+			#print(f"Filename {filename} | Module Code {module_code}")
 		elif menu == "3":
 			break
 		else:
