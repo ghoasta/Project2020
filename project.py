f3d50ae5fabdf7ed407aad8b2a09d8963ff5c5c5
Andrzej Sokolowski <ghoasta18@gmail.com>
2020-04-19 21:35:05 +0100

    v3 added saving to file

--- project.py
+++ project.py
@@ -29,7 +29,10 @@ def create_student_list(filename):
 	absent_list = str_list_to_int_list(absent_list)
 	excused_list = str_list_to_int_list(excused_list)
 
-	return student_list,present_list,absent_list,excused_list		
+	total_days= present_list[0]+excused_list[0]+absent_list[0]
+	target_percent = (70 * total_days)/100
+
+	return student_list,present_list,absent_list,excused_list,target_percent,total_days		
 
 def take_class_attendance(filename,module_code,purpose):
 	print(f"\nModule Record System({purpose}) {module_code}")
@@ -43,25 +46,60 @@ def take_class_attendance(filename,module_code,purpose):
 		print("3. Excused")
 		atten = input("> ")
 
+def non_attander(student_list,present_list):
+	non_attander =[]
+	for i in range(len(student_list)):
+		if present_list[i] == 0:
+			non_attander.append(student_list[i])
+	return non_attander
+
+def calc_seventy_percent(student_list,absent_list,total_days,target_percent):
+	below_seventy = []
+	for i in range(len(absent_list)):
+		if absent_list[i] > (total_days - target_percent):
+			below_seventy.append(student_list[i])
+	return below_seventy			
+
+def calc_best_attendance(student_list,present_list,total_days):
+	best_attendance = []
+	max_days = max(present_list)
+	for i in range(len(present_list)):
+		if present_list[i] == max_days:
+			best_attendance.append(student_list[i])
+	return	best_attendance		
+
+
 def get_class_attendance(filename,module_code,purpose):
-	student_list,present_list,absent_list,excused_list = create_student_list(filename)
-	print(f"Module: {module_code}")
-	print(f"Number of students: {len(student_list)}")
-	print(f"Number of classes: {max(present_list)}")
-	print(f"Avarage Attendance: {sum(present_list)/len(present_list)} days")
-	print(f"Low Attender(s): under 70.0 %")
-	print(f"")
-	print(f"Non Attender(s):")
-	print(f"")
-	print(f"Best Attender(s):")
-	print(f"")
-	print(present_list)
-	print(absent_list)
-	print(excused_list)
+	student_list,present_list,absent_list,excused_list,target_percent,total_days = create_student_list(filename)
 	new_filename = module_code+"_"+(datetime.date.today().strftime("%d-%m-%Y"))+".txt"
-	print(new_filename)
-
-
+	below = (', '.join(calc_seventy_percent(student_list,absent_list,total_days,target_percent)))
+	none = (', '.join(non_attander(student_list,present_list)))
+	best = (', '.join(calc_best_attendance(student_list,present_list,total_days)))
+	average = sum(present_list)/len(present_list)
+	output = open(new_filename,'w')
+	
+	print(f"Module: {module_code}")
+	print(f"Module: {module_code}",file=output)
+	print(f"Number of students: {len(student_list)}")
+	print(f"Number of students: {len(student_list)}",file=output)
+	print(f"Number of classes: {max(present_list)}")
+	print(f"Number of classes: {max(present_list)}",file=output)
+	print("Avarage Attendance: {:0,.1f} days".format(average))
+	print("Avarage Attendance: {:0,.1f} days".format(average),file=output)
+	print(f"Low Attender(s): under 70.0 %")
+	print(f"Low Attender(s): under 70.0 %",file=output)
+	print(f"\t{below}")
+	print(f"\t{below}",file=output)
+	print(f"Non Attender(s):")
+	print(f"Non Attender(s):",file=output)
+	print(f"\t{none}")
+	print(f"\t{none}",file=output)
+	print(f"Best Attender(s):")
+	print(f"Best Attender(s):",file=output)
+	print(f"\tAttended {max(present_list)}/{total_days} days")
+	print(f"\tAttended {max(present_list)}/{total_days} days",file=output)
+	print(f"\t{best}")
+	print(f"\t{best}",file=output)
 
 def load_module(purpose):
 	print(f"\nModule Record System({purpose}) - Choose a Module")
